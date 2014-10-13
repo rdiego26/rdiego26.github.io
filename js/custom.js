e61d06ca56939e485a20a88b2f79514a90749ab1
Diego Ramos <rdiego26@gmail.com>
2014-10-13 19:59:17 -0300

    CLOSE #1

--- js/custom.js
+++ js/custom.js
@@ -17,41 +17,79 @@ function sendMessage() {
         _website = jQuery("#website").val(),
         _message = jQuery("#message").val();
 
-    jQuery.ajax({
-        type: "POST",
-        url: 'http://diegoramos.me:4500/sendEmail',
-        data: {name: _name, email: _email, subject: _subject, website: _website, message: _message},
-        success: function (res) {
+    if(_name === '' || _name == undefined) {
+        jQuery("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">' +
+                '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
+                'Por favor, informe seu nome' +  '</div>'
+        );
+    } else if(!validateEmail(_email)) {
 
-            if (res && res.status.cod == 200) {
-                jQuery("#name").val('');
-                jQuery("#email").val('');
-                jQuery("#subject").val('');
-                jQuery("#website").val('');
-                jQuery("#message").val('');
+        jQuery("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">' +
+                '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
+                'Por favor, informe um e-mail válido' +  '</div>'
+        );
 
-                $("#messages").html('<div class="alert alert-success alert-dismissible" role="alert">' +
+    } else if(_website != '' || _website == undefined) {
+        if(!validateWebsite(_website)) {
+            jQuery("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">' +
+                '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
+                'Website informado inválido' +  '</div>');
+        }
+
+    } else if(_message === '' || _message === undefined) {
+        jQuery("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">' +
+                '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
+                'Por favor, informe a mensagem' +  '</div>'
+        );
+    } else {
+
+        jQuery.ajax({
+            type: "POST",
+            url: 'http://diegoramos.me:4500/sendEmail',
+            data: {name: _name, email: _email, subject: _subject, website: _website, message: _message},
+            success: function (res) {
+
+                if (res && res.status.cod == 200) {
+                    jQuery("#name").val('');
+                    jQuery("#email").val('');
+                    jQuery("#subject").val('');
+                    jQuery("#website").val('');
+                    jQuery("#message").val('');
+
+                    jQuery("#messages").html('<div class="alert alert-success alert-dismissible" role="alert">' +
+                            '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
+                            'Obrigado pelo contato, retornarei o mais breve possível' + '</div>'
+                    );
+
+                } else {
+                    jQuery("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">' +
+                            '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
+                            'Um problema ocorreu durante o envio da mensagem' +  '</div>'
+                    );
+                }
+
+            },
+            error: function (err) {
+                jQuery("#messages").html('<div class="alert alert-danger alert-dismissible" role="alert">' +
                         '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
-                        'Message sent, i will return as soon as possible' + '</div>'
-                );
-
-            } else {
-                $("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">' +
-                        '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
-                        'A problem occurred during sent message' +  '</div>'
+                        'Um problema ocorreu durante o envio da mensagem' +  '</div>'
                 );
             }
 
-        },
-        error: function (err) {
-            $("#messages").html('<div class="alert alert-danger alert-dismissible" role="alert">' +
-                    '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' +
-                    'A problem occurred during sent message' +  '</div>'
-            );
-        }
+        });
 
-    });
+    }
 
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function validateWebsite(website) {
+    var re = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+    return re.test(website);
 }
 
 /* ==============================================
@@ -114,7 +152,7 @@ $(function() {
         event.preventDefault();
     });
 });
-    
+
 
 
 /*=========================*/
